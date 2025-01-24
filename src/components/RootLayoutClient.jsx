f7ef56704f8ac9cfd2e207fbf39687fbd3611340
Jiyeong Kim <jiyeongkim@outlook.com>
2025-01-24 20:07:08 +0900

    padding 수정

--- src/components/RootLayoutClient.jsx
+++ src/components/RootLayoutClient.jsx
@@ -5,10 +5,10 @@ import Navigation from '@/components/Navigation'
 export default function RootLayoutClient({ children }) {
   return (
     <FormProvider>
-      <div className="min-h-screen bg-slate-100 py-10">
+      <div className="min-h-screen bg-slate-100 py-6">
         <div className="container max-w-screen-xl mx-auto">
           <div className=""><Navigation /></div>
-          <div className="bg-white mt-10 rounded-2xl">
+          <div className="bg-white mt-6 rounded-2xl">
             {children}
           </div>
         </div>
